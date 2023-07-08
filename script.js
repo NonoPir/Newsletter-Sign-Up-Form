5649b8afbfdebea4b78018e711fc0192bd7beca6
NonoPir <sanoonas100@gmail.com>
2023-07-08 14:24:12 +0400

    Refined JS code

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
+
 const invalidEmail = document.getElementById("invalid");
 const enterMail = document.getElementById("enter");
 const emailInput = document.getElementById("email-input");
@@ -7,33 +8,52 @@ const secondBox = document.querySelector(".second-box");
 const secondBoxChild = document.querySelector(".second-box-child");
 const secondBoxPara = document.getElementById("second-box-para");
 const button = document.getElementById("dismiss-button");
-const validateEmail = (email) => {
-  email = emailInput.value;
+
+const addHiddenClass = () => {
+  invalidEmail.classList.add("hidden");
+  enterMail.classList.add("hidden");
+  box.classList.add("hidden");
+  secondBox.classList.add("hidden");
+};
+
+const removeHiddenClass = () => {
+  secondBox.classList.remove("hidden");
+};
+
+const removeWarningClass = () => {
+  emailInput.classList.remove("warning");
+};
+
+const addWarningClass = () => {
+  emailInput.classList.add("warning");
+};
+
+const validateEmail = () => {
+  const email = emailInput.value;
   const matchEmail = String(email)
     .toLowerCase()
     .match(
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
+
   if (matchEmail) {
-    invalidEmail.classList.add("hidden");
-    emailInput.classList.remove("warning");
-    enterMail.classList.add("hidden");
-    box.classList.add("hidden");
-    secondBox.classList.remove("hidden");
+    addHiddenClass();
+    removeWarningClass();
     secondBoxPara.innerHTML = secondBoxPara.innerHTML.replace("blank", email);
+    removeHiddenClass();
   } else if (email === "") {
     enterMail.classList.remove("hidden");
-    emailInput.classList.add("warning");
+    addWarningClass();
     invalidEmail.classList.add("hidden");
   } else {
-    emailInput.classList.add("warning");
+    addWarningClass();
     invalidEmail.classList.remove("hidden");
     enterMail.classList.add("hidden");
   }
 };
 
 const dismissMessage = function () {
-  secondBox.classList.add("hidden");
+  addHiddenClass();
   box.classList.remove("hidden");
   emailInput.value = "";
 };
